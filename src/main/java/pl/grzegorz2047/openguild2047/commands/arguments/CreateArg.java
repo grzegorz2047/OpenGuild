d0624ef89be855051344a743ffb75bba829349a7
Grzegorz <Grzegorz@HomeBase>
2014-02-14 20:13:11 +0100

    Usunięcie "Błędnych argumentów" z powodu wyświetlania sie ich w przypadku nie spełnieniu pewnego warunku.

--- src/main/java/pl/grzegorz2047/openguild2047/commands/arguments/CreateArg.java
+++ src/main/java/pl/grzegorz2047/openguild2047/commands/arguments/CreateArg.java
@@ -24,6 +24,8 @@
 
 package pl.grzegorz2047.openguild2047.commands.arguments;
 
+import ca.wacos.nametagedit.NametagAPI;
+import ca.wacos.nametagedit.NametagEdit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import pl.grzegorz2047.openguild2047.GenConf;
@@ -64,6 +66,10 @@ public class CreateArg {
                         SimplePlayerGuild spg = new SimplePlayerGuild(p.getName(),sg.getTag(),true);
                         PluginData.getDataInstance().guilds.put(sg.getTag(), sg);
                         PluginData.getDataInstance().guildsplayers.put(p.getName(), spg);
+                        if(NametagAPI.hasCustomNametag(p.getName())){
+                            NametagAPI.resetNametag(p.getName());
+                        }
+                        NametagAPI.setNametagHard(p.getName(), sg.getTag(), "");
                         p.sendMessage(GenConf.prefix+MsgManager.createguildsuccess);
                         return true;
                     }else{
