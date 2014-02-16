d441569fa4a44d51651afc8fe41ed94e3ffd7f42
Grzegorz2047 <grzegorz2047@gmail.com>
2014-02-16 04:10:34 +0100

    test

--- src/main/java/pl/grzegorz2047/openguild2047/commands/arguments/DescriptionArg.java
+++ src/main/java/pl/grzegorz2047/openguild2047/commands/arguments/DescriptionArg.java
@@ -21,7 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-
+  //
 package pl.grzegorz2047.openguild2047.commands.arguments;
 
 import org.bukkit.command.CommandSender;
