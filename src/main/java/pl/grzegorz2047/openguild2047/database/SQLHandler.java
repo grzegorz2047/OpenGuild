849fce1338352dc7d484c027b037d4a4fd4ed935
Grzegorz <grzegorz2047@gmail.com>
2017-08-07 00:09:57 +0100

    Poprawki bledow w konsoli.

--- src/main/java/pl/grzegorz2047/openguild2047/database/SQLHandler.java
+++ src/main/java/pl/grzegorz2047/openguild2047/database/SQLHandler.java
@@ -109,7 +109,9 @@ public class SQLHandler {
                     + "(tag VARCHAR(11),"
                     + "description VARCHAR(100),"
                     + "leader VARCHAR(37),"
+                    + "lives INT,"
                     + "home_x INT,"
+                    + "home_y INT,"
                     + "home_z INT,"
                     + "home_world VARCHAR(16),"
                     + "PRIMARY KEY(tag));";
